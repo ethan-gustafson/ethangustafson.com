6f0bdf6f6b42374acaf839183df51e4c08bb46cb
Ethan <ethan_gustafson@outlook.com>
2020-10-12 21:58:26 -0400

    styled h1, h2, and portfolio link in styled component in Intro.js

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Section, TextContainer } from "./styles/StyledIntro.js";
+import { Section, TextContainer, Name, Career, P, PortfolioLink } from "./styles/StyledIntro.js";
 import { IntroBackgroundImage } from "./images/IntroBackgroundImage.js";
 
 const Intro = () => (
   <Section>
     <IntroBackgroundImage>
       <TextContainer>
-        <h1 style={{fontSize: "70px", textShadow: "white 0px 1px 1px"}}>Ethan Gustafson</h1>
-        <h2 style={{fontSize: "50px"}}>Software Engineer</h2>
+        <Name>Ethan Gustafson</Name>
+        <Career>Software Engineer</Career>
+        <P>
+          <PortfolioLink to="#portfolio">View My Work</PortfolioLink>
+        </P>
       </TextContainer>
     </IntroBackgroundImage>
   </Section>
--- src/components/main/styles/StyledIntro.js
+++ src/components/main/styles/StyledIntro.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link as A } from 'gatsby';
 import styled from 'styled-components';
 
 export const Section = styled.section`
@@ -17,4 +19,25 @@ export const TextContainer = styled.div`
 
   margin: 0 auto;
   text-align: center;
+`
+export const Name = styled.h1`
+  font-size: 70px;
+  text-shadow: white 0px 1px 1px;
+`
+
+export const Career = styled.h2`
+  font-size: 50px;
+`
+
+export const PortfolioLink = styled(props => (<A {...props}/>))`
+  color: white;
+  background-color: #ef233c;
+  text-decoration: none;
+
+  border-radius: 20px;
+  padding: 15px;
+`
+
+export const P = styled.p`
+  margin-top: 1%;
 `
