b54484e38708a00a533bb7a46516106f2f66f97f
Ethan <ethan_gustafson@outlook.com>
2020-10-12 22:10:10 -0400

    added barebones portfolio component, styled section, h2, projects container and project div in StyledPortfolio.js

---
+++ src/components/main/Portfolio.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Section, PortfolioH2, ProjectsContainer, Project } from "./styles/StyledPortfolio.js";
+
+
+const Portfolio = () => (
+  <Section>
+    <PortfolioH2>Portfolio</PortfolioH2>
+    <ProjectsContainer>
+      <Project></Project>
+
+      <Project></Project>
+
+      <Project></Project>
+
+      <Project></Project>
+      
+      <Project></Project>
+    </ProjectsContainer>
+  </Section>
+)
+
+export default Portfolio;
---
+++ src/components/main/styles/StyledPortfolio.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  background-color: #08090F;
+`
+
+export const PortfolioH2 = styled.h2`
+  font-size: 50px;
+  color: white;
+  text-align: center;
+`
+
+export const ProjectsContainer = styled.div`
+  width: 80%;
+  display: flex;
+`
+
+export const Project = styled.div`
+  border-radius: 4px;
+`
