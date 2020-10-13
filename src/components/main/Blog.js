9ed0fa8e4365ae79bf2e1d9a11705b60bf551b5a
Ethan <ethan_gustafson@outlook.com>
2020-10-13 13:04:45 -0400

    finished blog section

--- src/components/main/Blog.js
+++ src/components/main/Blog.js
@@ -4,25 +4,42 @@ import {
   BlogH2, 
   BlogsContainer,
   BlogDiv,
-  BlogImage,
-  BlogInfo 
+  IFrame
 } from "./styles/StyledBlog.js";
 
-const Blog = () => (
-  <Section>
-    <BlogH2 id="blog">Blog</BlogH2>
-    <BlogsContainer>
-      <BlogDiv>
-        <BlogImage></BlogImage>
-        <BlogInfo></BlogInfo>
-      </BlogDiv>
-      <BlogDiv></BlogDiv>
-      <BlogDiv></BlogDiv>
-      <BlogDiv></BlogDiv>
-      <BlogDiv></BlogDiv>
-      <BlogDiv></BlogDiv>
-    </BlogsContainer>
-  </Section>
-)
+const Blog = () => {
+  return (
+    <Section>
+      <BlogH2 id="blog">Blog</BlogH2>
+      <BlogsContainer>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/simple-recursion-challenges-ruby-js-python-55j7" />
+        </BlogDiv>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/c-environment-configuration-vs-code-macos-30ga" />
+        </BlogDiv>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/intro-to-learning-c-20eo" />
+        </BlogDiv>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/rspec-pt-3-test-doubles-3mml" />
+        </BlogDiv>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/css-float-flexbox-14fg" />
+        </BlogDiv>
+
+        <BlogDiv>
+          <IFrame src="https://dev.to/ethanmgustafson/simple-guide-to-css-grid-3cja" />
+        </BlogDiv>
+
+      </BlogsContainer>
+    </Section>
+  )
+}
 
 export default Blog;
--- src/components/main/styles/StyledBlog.js
+++ src/components/main/styles/StyledBlog.js
@@ -34,29 +34,8 @@ export const BlogDiv = styled.div`
   border-radius: 20px;
 `
 
-export const ImgContainer = styled.div`
+export const IFrame = styled.iframe`
   width: 100%;
-  height: 200px;
-  &:hover{
-    height: 260px;
-    transition-duration: 0.4s;
-  }
-`
-
-export const BlogImage = styled(props => (<Img {...props}/>))`
-  object-fit: "cover";
   height: 100%;
-  width: 100%;
-  border-radius: 20px 20px 0 0;
-`
-
-export const Title = styled.h3`
-  margin-top: 10px;
-  text-align: center;
-`
-
-export const BlogInfo = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  text-align: left;
+  border-radius: 20px;
 `
