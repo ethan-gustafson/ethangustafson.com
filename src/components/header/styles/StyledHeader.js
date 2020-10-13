b8cca1c054293c990dae5bc9d6013bede5b1a3da
Ethan <ethan_gustafson@outlook.com>
2020-10-13 10:48:55 -0400

    adjusted body and header so that header is sticky throughout entire page scroll

--- src/components/header/styles/StyledHeader.js
+++ src/components/header/styles/StyledHeader.js
@@ -4,4 +4,7 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
   background-color: #151514;
   height: 80px;
+  position: sticky;
+  top: 0;
+  z-index: 2;
 `
--- src/components/main/styles/StyledIntro.js
+++ src/components/main/styles/StyledIntro.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   width: 100%;
-  height: 100%;
+  height: 100vh;
 `
 
 export const TextContainer = styled.div`
