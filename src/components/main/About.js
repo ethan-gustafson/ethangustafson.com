0c4ad7806e26bee88ad85beda9392517350fae3d
Ethan <ethan_gustafson@outlook.com>
2020-10-14 02:21:24 -0400

    added generic about section, will update later

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -11,8 +11,8 @@ const About = props => (
       </ImgContainer>
 
       <InfoContainer>
-        <P>I am a Software Engineer based in Daytona Beach, Florida.</P>
-        <P>Software engineer transitioning a technical and creative background in filmmaking to the field of web development. My former work required a highly complex and diverse skill set, including the mastery of high-end software editing platforms, hardware tools, client-facing customer service, and project management. Working with Rails and React is a natural step in my career path.</P>
+        <P>Software Engineer based in Daytona Beach, Florida.</P>
+        <P>I am transitioning a technical and creative background in filmmaking to the field of web development. My former work required a highly complex and diverse skill set, including the mastery of high-end software editing platforms, hardware tools, client-facing customer service, and project management. Working with Rails and React is a natural step in my career path.</P>
       </InfoContainer>
 
     </MainContainer>
