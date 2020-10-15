8276dbb173047abb154339149715ef19f507384b
Ethan <ethan_gustafson@outlook.com>
2020-10-15 19:29:08 -0400

    updated font family for portfolio

--- src/components/main/styles/StyledAboutMain.js
+++ src/components/main/styles/StyledAboutMain.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledAboutMain = styled.main`
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', georgia, serif, sans-serif;
+  font-family: arial, georgia, serif, sans-serif;
   width: 100%;
   height: 100%;
 `
--- src/components/main/styles/StyledMain.js
+++ src/components/main/styles/StyledMain.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', georgia, serif, sans-serif;
+  font-family: arial, georgia, serif, sans-serif;
   width: 100%;
   height: 4220px;
   @media(max-width: 650px) {
